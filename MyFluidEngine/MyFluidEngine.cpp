235ae892b9de1a8ab7c15e875669ca8e45733364
“PPPoseidon” <947205526@qq.com>
2021-09-27 10:21:56 +0800

    test

---
+++ MyFluidEngine/MyFluidEngine.cpp
@@ -0,0 +1,22 @@
+﻿// MyFluidEngine.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MyFluidEngine.h"
+
+
+// 这是导出变量的一个示例
+MYFLUIDENGINE_API int nMyFluidEngine=0;
+
+// 这是导出函数的一个示例。
+MYFLUIDENGINE_API int fnMyFluidEngine(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+CMyFluidEngine::CMyFluidEngine()
+{
+    return;
+}
